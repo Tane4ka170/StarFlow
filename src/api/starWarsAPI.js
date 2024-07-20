2515190cace1ab7d9171ddd56a83259dae1c9375
Tane4ka170 <tane4ka170@gmail.com>
2024-07-20 22:00:17 +0300

    updated api

--- src/api/starWarsAPI.js
+++ src/api/starWarsAPI.js
@@ -3,11 +3,21 @@ import axios from 'axios';
 const API_BASE_URL = 'https://sw-api.starnavi.io';
 
 export const fetchHeroes = async (page = 1) => {
-  const response = await axios.get(`${API_BASE_URL}/people/?page=${page}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_BASE_URL}/people/?page=${page}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching heroes:', error);
+    throw error;
+  }
 };
 
 export const fetchHeroDetails = async (id) => {
-  const response = await axios.get(`${API_BASE_URL}/people/${id}/`);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_BASE_URL}/people/${id}/`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching hero details for ID ${id}:`, error);
+    throw error;
+  }
 };
