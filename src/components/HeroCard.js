247aeaf591be54e708467a113ddbf5a88483ee81
Tane4ka170 <tane4ka170@gmail.com>
2024-07-21 17:47:53 +0300

    updated HeroCard

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Badge, Box, HStack, Text, VStack } from '@chakra-ui/react';
 
 const HeroCard = ({ hero }) => {
   const router = useRouter();
@@ -13,17 +13,26 @@ const HeroCard = ({ hero }) => {
 
   return (
     <Box
-      onClick={handleClick}
-      cursor="pointer"
       borderWidth="1px"
       borderRadius="lg"
       overflow="hidden"
+      p="6"
+      bg="white"
+      _hover={{ bg: 'gray.100', cursor: 'pointer' }}
+      onClick={handleClick}
+      w="100%"
     >
-      <Image src={hero.image} alt={hero.name} />
-      <Box p="6">
-        <Text fontSize="xl">{hero.name}</Text>
-        <Text>{hero.description}</Text>
-      </Box>
+      <VStack align="start">
+        <HStack justifyContent="space-between" w="100%">
+          <Text fontWeight="bold" fontSize="xl">
+            {hero.name}
+          </Text>
+          <Badge colorScheme="teal">Hero</Badge>
+        </HStack>
+        <Text fontSize="md">Height: {hero.height}</Text>
+        <Text fontSize="md">Mass: {hero.mass}</Text>
+        <Text fontSize="md">Gender: {hero.gender}</Text>
+      </VStack>
     </Box>
   );
 };
